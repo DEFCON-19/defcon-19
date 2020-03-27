db6a14d14717c048e39e2538e7551665ec66af91
Adrian Koxha <koxhaadrian@gmail.com>
2020-03-27 22:08:32 +0100

    initial commit

---
+++ server.R
@@ -0,0 +1,18 @@
+server <- function(input, output) {
+  sourceDirectory("sections", recursive = TRUE)
+
+  showNotification("Attention: Johns Hopkins University does not provide recovered cases anymore. Therefore,
+  recovered cases are an estimation only. Please check the about section for more information.",
+    duration = NULL, type = "error")
+
+  # Trigger once an hour
+  dataLoadingTrigger <- reactiveTimer(3600000)
+
+  observeEvent(dataLoadingTrigger, {
+    updateData()
+  })
+
+  observe({
+    data <- data_atDate(input$timeSlider)
+  })
+}
